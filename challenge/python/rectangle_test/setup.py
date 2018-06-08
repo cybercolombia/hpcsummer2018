83ca7ad3e48ada22ed866307da21a1bf8f85196e
Juan P Mallarino <jp.mallarino50@uniandes.edu.co>
2018-06-07 23:26:09 -0500

    LOCAL: removed rectangle_test directory for python completion

--- challenge/python/rectangle_test/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-
-setup(
-	ext_modules=[Extension("rectangle", ["rectangle.pyx", "rectangle_c.cpp"], language="c++",)],
-	cmdclass = {'build_ext': build_ext}
-)
